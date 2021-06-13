df397c65b1b115d04486f2ec4c395ef6a9b2412c
Fernando <46842299+fersgva@users.noreply.github.com>
2021-06-13 17:54:44 +0200

    Combate enemigo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,9 +16,14 @@ public class Enemy : MonoBehaviour
     float navRadius = 10;
     float minDistanceToNewPoint = 3;
     Animator anim;
-    enum State { patrol, combat};
+    enum State { patrol, chase, combat};
     State enemState;
     GameObject target;
+    Vector3 dirToTarget;
+    bool playerOnSight;
+    float combatTimer;
+    float timeForNextAttack;
+    float meleeDistance = 1.4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,42 +38,82 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+        if(enemState == State.chase)
+        {
+            agent.speed = 4f;
+            agent.SetDestination(target.transform.position);
+            if(agent.remainingDistance <= meleeDistance) //Nos ha alcanzado
+            {
+                agent.isStopped = true;
+                anim.SetBool("combatIdle", true);
+                timeForNextAttack = Random.Range(0f, 4f); //Tiempo random para el primer ataque a lanzar.
+                enemState = State.combat;
+            }
+        }
+        else if(enemState == State.combat)
+        {
+            dirToTarget = target.transform.position - transform.position;
+            transform.forward = dirToTarget.normalized;
+            agent.SetDestination(target.transform.position);
+            combatTimer += Time.deltaTime;
+            if (agent.remainingDistance <= meleeDistance) //Si la distancia sigue siendo melee
+            {
+                agent.isStopped = true;
+                if(combatTimer >= timeForNextAttack)
+                {
+                    anim.SetTrigger("attack");
+                    timeForNextAttack = Random.Range(0f, 4f); //Déspués de cada ataque, calculamos el tiempo para el siguinete.
+                    combatTimer = 0;
+                }
+            }
+            else if (agent.remainingDistance  > 1.5f && !info.IsName("EnemyAttack"))
+            {
+                agent.isStopped = false;
+                anim.SetBool("combatIdle", false);
+                enemState = State.chase;
+            }
+        }
     }
     private void FixedUpdate()
     {
         if(enemState == State.patrol)
         {
-            Collider[] colls = Physics.OverlapSphere(head.transform.position, enemView.viewRadius, isDetectable);
-            if (colls.Length > 0) //Player detectado dentro de nuestro radio.
+            if(!playerOnSight)
             {
-                target = colls[0].gameObject;
-                Vector3 dirToTarget = target.transform.position - head.transform.position;
-                if (Vector3.Angle(head.transform.forward, dirToTarget.normalized) <= enemView.viewAngle / 2) //Player en nuestro cono de visión.
+                Collider[] colls = Physics.OverlapSphere(head.transform.position, enemView.viewRadius, isDetectable);
+                if (colls.Length > 0) //Player detectado dentro de nuestro radio.
                 {
-                    //Y si NO hay obstáculo entre donde estoy y el player....
-                    if (!Physics.Raycast(head.transform.position, dirToTarget, dirToTarget.magnitude, isObstacle))
+                    target = colls[0].gameObject;
+                    dirToTarget = target.transform.position - head.transform.position;
+                    if (Vector3.Angle(head.transform.forward, dirToTarget.normalized) <= enemView.viewAngle / 2) //Player en nuestro cono de visión.
                     {
-                        enemState = State.combat;
-                        StopAllCoroutines();
-                        anim.SetBool("looking", false); //por si esaba mirando.
-                        transform.forward = dirToTarget;
-                        anim.SetTrigger("alert");
-                        agent.isStopped = true;
+                        //Y si NO hay obstáculo entre donde estoy y el player....
+                        if (!Physics.Raycast(head.transform.position, dirToTarget, dirToTarget.magnitude, isObstacle))
+                        {
+                            StopAllCoroutines();
+                            anim.SetBool("looking", false); //por si estaba mirando.
+                            anim.SetTrigger("alert");
+                            agent.isStopped = true;
+                            playerOnSight = true;
+                        }
                     }
                 }
             }
-        }
-        else if(enemState == State.combat)
-        {
-            agent.SetDestination(target.transform.position);
+            else //Para hacer el seguimiento visual mientras dura la animación de Alert.
+            {
+                dirToTarget = target.transform.position - head.transform.position;
+                transform.forward = dirToTarget.normalized;
+            }
+            
         }
     }
 
     void OnAlertFinish()
     {
         agent.isStopped = false;
-        agent.stoppingDistance = 2;
+        agent.stoppingDistance = meleeDistance;
+        enemState = State.chase;
     }
     IEnumerator NavigateToNewDest()
     {
